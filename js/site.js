0bac2416970ea888ecfc90b8b9c2d5c6c518972f
chrismillsnbcu <chris.mills@nbcuni.com>
2014-12-19 16:33:54 -0800

    new key

--- js/site.js
+++ js/site.js
@@ -1,5 +1,5 @@
 /***** MPS Demo - NBC News.com waterfall *****/
-var API_KEY = 'AIzaSyDi67W8JBn2RGPFTKH97Cin0Jm_9frKB-0';
+var API_KEY = 'AIzaSyB-guPk6KtXj4a1k4ePyIh4CWVZJxLaaDo';
 var URL_TO_GET_RESULTS_FOR = 'http://code.google.com/speed/page-speed/';
 
 var API_URL = 'https://www.googleapis.com/pagespeedonline/v1/runPagespeed?';
